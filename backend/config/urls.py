941e0b810a4312d89b793f00019dfe464da48dbe
Kelvin Omini <kelvin@example.com>
2026-06-24 10:58:00 +0100

    modified:   backend/config/urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -10,6 +10,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('accounts/', include('allauth.urls')),
     
     # Versioned REST API
     path('api/v1/', include('config.api_urls')),
